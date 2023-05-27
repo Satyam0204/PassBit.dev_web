454df3adaab3e8de7bbc79fe7f96041e6098fe6b
Satyam0204 <satyampattanaik007@gmail.com>
2023-05-28 00:51:41 +0530

    [ADD] added initial files in next project

---
+++ passbit.dev-web/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+     <div>
+      This is the best password managing site you could ever get!!!
+     </div>
+    </main>
+  )
+}
