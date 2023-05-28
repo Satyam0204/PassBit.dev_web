f9dc7b7b27e57937fd677781232d44264ad02a89
ROUNAK-K-JHA-2002 <rounakjha291202.rar@gmail.com>
2023-05-28 11:45:30 +0530

    [ADD] Setup Client-Side Home Routes

---
+++ app/home/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+    This is the best password managing site you could ever get!!!
+   </div>
+  )
+}
+
+export default Dashboard
--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
-import Image from 'next/image'
 import styles from './page.module.css'
+import Dashboard from './home/page'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     <div>
-      This is the best password managing site you could ever get!!!
-     </div>
+     <Dashboard/>
     </main>
   )
 }
