04f0d30c06f957996518781c97bc7068a55d55f6
ROUNAK-K-JHA-2002 <rounakjha291202.rar@gmail.com>
2023-06-02 17:52:57 +0530

    Build Navbar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 function MobileNav({open,setOpen}){
     return (
-        <div className={` text-white absolute top-0 left-0 h-screen w-screen transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
+        <div className={` bg-slate-900 z-20 text-white absolute top-0 left-0 h-screen w-screen transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
 
             <div className="h-20"> 
                
@@ -37,12 +37,14 @@ function Navbar(){
     return (
         <nav  className=" text-white flex filter drop-shadow-md  px-4 py-2 h-20.5 items-center border-solid border-b-2 border-sky-200">
             <MobileNav open={open} setOpen={setOpen}/>
-            <div className="w-2/12 flex items-center mx-2">
-                <a className="text-2xl font-semibold my-3" href="/"><Image src="/assets/passbit-website-navbar.png" width={170} height={170}/></a>
+            <div className="w-5/12 md:w-1/12 flex items-center mx-2 ">
+                <a className="text-2xl font-semibold my-3" href="/">
+                <img src="/assets/passbit-website-navbar.png"  style={{"height" : "3vh"}} alt="" />
+                    </a>
             </div>
-            <div className="w-10/12 flex justify-end md:justify-center items-center">
+            <div className="w-10/12 flex justify-end md:justify-center items-center ">
 
-                <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden" onClick={() => {
+                <div className="z-50 flex relative w-8 h-6 flex-col justify-between items-center md:hidden" onClick={() => {
                     setOpen(!open)
                 }}>
                     {/* hamburger button */}
