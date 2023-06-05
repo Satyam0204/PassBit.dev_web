082858d12a05861c2b5a1f5b0b4c1d01446542a5
ROUNAK-K-JHA-2002 <rounakjha291202.rar@gmail.com>
2023-06-05 10:51:59 +0530

    Refactoring Changes

--- app/home/page.js
+++ app/home/page.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Landing from '../../src/components/Dashboard/Landing'
+import Features from '../../src/components/Dashboard/Features'
+import PassBreachInfo from '../../src/components/Dashboard/password_breach'
 
 function Dashboard() {
   return (
   <>
   <Landing />
+  <PassBreachInfo />
+  <Features />
   </>
   )
 }
